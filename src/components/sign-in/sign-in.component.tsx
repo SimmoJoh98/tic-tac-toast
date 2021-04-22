121a4fa8709f163c5ae77ef466b58c002d659a7b
Xeilon <simmojoh98@gmail.com>
2021-04-21 20:15:59 -0600

    rearranging

--- src/components/sign-in/sign-in.component.tsx
+++ src/components/sign-in/sign-in.component.tsx
@@ -6,20 +6,22 @@ function Auth () {
     const [data, setData] = useState({Username: '', Password:''})
     
    const handleSubmit = () => {
+    //epic dubbugging skills below. v
     //console.log(data.Username);
     //console.log(data.Password);
-    let userData = {};
-    const res = axios.get('https://jsonplaceholder.typicode.com/todos/1').then(() => {userData = res});
+
+    let userData = data;
+
    }
 
     return(
         <div>
+            
             <input placeholder='Username' type='text' value={data.Username} onChange={e => setData({ ...data, Username: e.target.value})} />
             <input placeholder='Password' type='text' value={data.Password} onChange={e => setData({ ...data, Password: e.target.value})} />
             <button onClick={handleSubmit} type="submit">Login</button>
         </div>
     )
 }
-
 
 export default Auth;
