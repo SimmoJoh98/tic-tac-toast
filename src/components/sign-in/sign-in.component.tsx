b4dcae538465f2ea5a545700876b64ba146bc7a6
Xeilon <simmojoh98@gmail.com>
2021-05-26 04:14:41 -0600

    Update sign-in.component.tsx

--- src/components/sign-in/sign-in.component.tsx
+++ src/components/sign-in/sign-in.component.tsx
@@ -6,18 +6,14 @@ function Auth () {
     const [data, setData] = useState({Username: '', Password:''})
     
    const handleSubmit = () => {
-    //epic dubbugging skills below. v
-    //console.log(data.Username);
-    //console.log(data.Password);
-
-    let userData = data;
+    
    }
 
     return(
         <div>
             <input placeholder='Username' type='text' value={data.Username} onChange={e => setData({ ...data, Username: e.target.value})} />
             <input placeholder='Password' type='text' value={data.Password} onChange={e => setData({ ...data, Password: e.target.value})} />
-            <button onClick={handleSubmit} type="submit">Login</button>
+            <button onClick={handleSubmit} type="submit">Sign In</button>
         </div>
     )
 }
