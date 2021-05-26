31ba914be56c1bc867d5f0e4e42d6256c9b22df6
Xeilon <simmojoh98@gmail.com>
2021-05-26 04:57:27 -0600

    fixing styles

--- src/components/game-board/game-board.component.tsx
+++ src/components/game-board/game-board.component.tsx
@@ -7,6 +7,7 @@ function GameBoard(){
         //[1,2,3]
         //[4,5,6]
         //[7,8,9]
+         playerTurn: 1,
          player1: 1,
          player2: 2,
          slot1: 0,
@@ -25,16 +26,19 @@ function GameBoard(){
     }
 
     return (
-        <div className='backboard'>
-            <div id='s1'>{state.slot1}</div>
-            <div id='s2'>{state.slot2}</div>
-            <div id='s3'>{state.slot3}</div>
-            <div id='s4'>{state.slot4}</div>
-            <div id='s5'>{state.slot5}</div>
-            <div id='s6'>{state.slot6}</div>
-            <div id='s7'>{state.slot7}</div>
-            <div id='s8'>{state.slot8}</div>
-            <div id='s9'>{state.slot9}</div>
+        <div className='gameboard'>
+            <div className='backboard'>
+                <div id='s1'>{state.slot1}</div>
+                <div id='s2'>{state.slot2}</div>
+                <div id='s3'>{state.slot3}</div>
+                <div id='s4'>{state.slot4}</div>
+                <div id='s5'>{state.slot5}</div>
+                <div id='s6'>{state.slot6}</div>
+                <div id='s7'>{state.slot7}</div>
+                <div id='s8'>{state.slot8}</div>
+                <div id='s9'>{state.slot9}</div>
+            </div>
+            <h1 id='turn'>{state.playerTurn}</h1>
         </div>
     )
 }
