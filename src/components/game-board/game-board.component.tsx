2d5482bc7a1e651756f778050dd7e21848b3c40c
Xeilon <simmojoh98@gmail.com>
2021-06-18 16:22:39 -0600

    Stopped burning my retinas with the color white

--- src/components/game-board/game-board.component.tsx
+++ src/components/game-board/game-board.component.tsx
@@ -22,19 +22,21 @@ function GameBoard(){
     }
 
     return (
-        <div className='gameboard'>
-            <div className='backboard'>
-                <div onClick={handleUserInput} id='s1'>{state.grid[0]}</div>
-                <div id='s2'>{state.grid[1]}</div>
-                <div id='s3'>{state.grid[2]}</div>
-                <div id='s4'>{state.grid[3]}</div>
-                <div id='s5'>{state.grid[4]}</div>
-                <div id='s6'>{state.grid[5]}</div>
-                <div id='s7'>{state.grid[6]}</div>
-                <div id='s8'>{state.grid[7]}</div>
-                <div id='s9'>{state.grid[8]}</div>
+        <div className="background">
+            <div className='gameboard'>
+                <div className='backboard'>
+                    <div onClick={handleUserInput} id='s1'>{state.grid[0]}</div>
+                    <div id='s2'>{state.grid[1]}</div>
+                    <div id='s3'>{state.grid[2]}</div>
+                    <div id='s4'>{state.grid[3]}</div>
+                    <div id='s5'>{state.grid[4]}</div>
+                    <div id='s6'>{state.grid[5]}</div>
+                    <div id='s7'>{state.grid[6]}</div>
+                    <div id='s8'>{state.grid[7]}</div>
+                    <div id='s9'>{state.grid[8]}</div>
+                </div>
+                <h1 id='turn'>It's player {state.playerTurn}'s turn</h1>
             </div>
-            <h1 id='turn'>It's player {state.playerTurn}'s turn</h1>
         </div>
     )
 }
