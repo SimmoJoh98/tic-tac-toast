b0b5c1cab859b94d1a1055127b6bb77cc9a9bb79
Xeilon <simmojoh98@gmail.com>
2021-06-25 18:08:05 -0600

    Update sign-in.component.tsx

--- src/components/sign-in/sign-in.component.tsx
+++ src/components/sign-in/sign-in.component.tsx
@@ -16,8 +16,8 @@ function Auth () {
 
     return(
         <div>
-            <input placeholder='Username' type='text' value={data.Username} onChange={e => setData({ ...data, Username: e.target.value})} />
-            <input placeholder='Password' type='text' value={data.Password} onChange={e => setData({ ...data, Password: e.target.value})} />
+            <input className="Username" placeholder='Username' type='text' value={data.Username} onChange={e => setData({ ...data, Username: e.target.value})} />
+            <input className="Password" placeholder='Password' type='text' value={data.Password} onChange={e => setData({ ...data, Password: e.target.value})} />
             <button onClick={handleSubmit} type="submit">Sign In</button>
         </div>
     )
